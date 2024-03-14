0a1db372b1a6e35f4e9a5f8c55893476cbc67a06
LILLK-DAW2 <tahrat.mehdi.co@alumnes.ilernalleida.com>
2024-03-14 20:19:32 +0100

    a
    a

--- Mijuru/Mijuru-Angular/src/app/components/campus/campusCrud/crear-campus/crear-campus.component.ts
+++ Mijuru/Mijuru-Angular/src/app/components/campus/campusCrud/crear-campus/crear-campus.component.ts
@@ -9,13 +9,23 @@ import {FormBuilder, FormGroup} from "@angular/forms";
   styleUrls: ['./crear-campus.component.css']
 })
 export class CrearCampusComponent implements OnInit {
+  imageUrl: string | ArrayBuffer | null = null;
 
   constructor(private _formBuilder: FormBuilder) {}
   firstFormGroup: FormGroup = this._formBuilder.group({firstCtrl: ['']});
   secondFormGroup: FormGroup = this._formBuilder.group({secondCtrl: ['']});
   ngOnInit(): void {
   }
-
+  onFileSelected(event: any) {
+    const file: File = event.target.files[0];
+    if (file) {
+      const reader = new FileReader();
+      reader.readAsDataURL(file);
+      reader.onload = () => {
+        this.imageUrl = reader.result;
+      };
+    }
+  }
 
 
 
